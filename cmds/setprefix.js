a7e1c9a2606c43bc802a241d05dff3aad4d6ba83
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-21 19:30:35 +0800

    Update setprefix.js

--- cmds/setprefix.js
+++ cmds/setprefix.js
@@ -1,19 +1,30 @@
-const Discord = require('discord.js');
-const prefixes = require('../prefixes.json');
+const Discord = require("discord.js");
 const fs = require("fs");
 
-exports.run = (bot, message, args) => {
-    const prefix = prefixes[message.guild.id].prefix;
-     if (!message.member.hasPermission("MANAGE_GUILD") && message.author.id !== "262410813254402048" &&  message.author.id !== "265279363199533068") return message.channel.send(`__**Access Denied**__\nYou must have __MANAGE_GUILD__ perms to use this command.`); 
-    if (!args) return message.channel.send("***You Did Not Set A Prefix! Please Do***!");
-    prefixes[message.guild.id].prefix = `${args}`
+exports.run = async (bot, message, args, color) => {
 
-    message.channel.send(`***The New Prefix For ${message.guild.name} Is Now ${args}!!!***`)
+    if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send("**Anda tidak ada permission untu melakukan command ini!");
+    if (!args[0] || args[0] == "help") return message.reply(`**Pakai : alu!setprefix <prefix baru>**`);
+    
+    let prefixes = JSON.parse(fs.readFileSync('./prefixes.json', 'utf8'));
+    prefixes[message.guild.id] = {
+        prefixes: args[0]
+    };
+
+    fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+        if (err) console.log(err)
+    });
+
+    let embed = new Discord.RichEmbed()
+    .setColor(color)
+    .setTitle("**Prefix Sudah Diterapkan!**")
+    .setDescription(`Prefix Diubah Menjadi **${args[0]}**`);
+
+    message.channel.send(embed);
 }
 
 exports.help = {
-    name: "setprefix",
-    description: "Sets the prefix of the server",
-    usage: `pr!setprefix [prefix]`,
-    note: "Only people with the permission MANAGE_GUILD can use this!"
+    name: "prefix",
+    description: "Change your server prefix",
+    usage: "suzu prefix <new prefix>"
 }
