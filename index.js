4ffa5593ba3aa7df96c25ef649cca2cd4d45bc8e
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-21 13:09:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,93 +1,101 @@
-const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: false});
+const cfg = require("./botconfig.json");
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
 
-fs.readdir("./cmds/", (err, files) => {
+bot.on("message", async message => {
 
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
+    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+    if (!prefixes[message.guild.id]) {
+        prefixes[message.guild.id] = {
+            prefixes: cfg.prefix
+        };
+    }
+    let prefix = prefixes[message.guild.id].prefixes;
+    let msg = message.content.toLowerCase();
+    let sender = message.author;
+    let args = message.content.slice(prefix.length).trim().split(" ");
+    let cmd = args.shift().toLowerCase();
 
-  jsfile.forEach((f, i) =>{
-    let props = require(`./cmds/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-});
+    if (message.content === `<@${bot.user.id}>`) {
+        message.channel.send(`Hi <@${message.author.id}>, my prefix is \`${prefix}\``);
+        message.react('👌');
+    }
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("?", {type: "WATCHING"});
+    if (sender.bot) return;
+    if (!message.content.startsWith(prefix)) return;
 
+    try {
+        let commandFile = require(`./cmds/${cmd}.js`);
+        commandFile.run(bot, message, args);
+    } catch(e) {
+        console.log(e.message);
+    } finally {
+        console.log(`${message.author.username} | ${cmd} | ${message.guild.name}`);
+    }
 });
 
 bot.on("guildMemberAdd", member => {                                                                     
-  const log = bot.channels.find("name", "member-log")
-  log.send(`${member} Telah Memasuki Server ${member.guild.name} Owner server ${member.guild.owner.user.tag} !`)
-  
+    const log = bot.channels.find("name", "member-log")
+    log.send(`${member} Telah Memasuki Server ${member.guild.name} Owner server ${member.guild.owner.user.tag} !`)
+    
 })
-
+  
 bot.on("guildMemberRemove", member => {
-
-  const log = bot.channels.find("name", "member-log")
-  log.send(`${member} Keluar dari server ${message.guild.name} Owner server ${member.guild.owner.user.tag} !`)
-})
-
-bot.on("channelCreate", channel => {
-
-if (channel.type == 'dm') return;
-const log = bot.channels.find("name", "admin-log")
-var embed = new Discord.RichEmbed()
-.setTitle("Membuat Channel!")
-.setColor("RANDOM")
-.setTimestamp()
-.addField(`Nama channel : ${channel.name}`, `Channel dibuat diserver ${channel.guild.name}`)
-log.send({ embed: embed })
-});
-
-bot.on("channelDelete", channel => {
-const log = bot.channels.find("name", "admin-log")
-var embed = new Discord.RichEmbed()
-.setTitle("Menghapus Channel!")
-.setColor("RANDOM")
-.setTimestamp()
-.setThumbnail(`${channel.guild.iconURL}`)
-.addField(`Nama channel : ${channel.name}`, `Channel dihapus diserver ${channel.guild.name}`)
-log.send({ embed: embed })
-});
-
-bot.on("guildCreate", guild => {
-  const guildOwner = guild.owner.user.tag;
   
-  const log = bot.channels.find("name", "server-log")
-  var embed2 = new Discord.RichEmbed()
-  .setTitle("Memasuki Server!")
+    const log = bot.channels.find("name", "member-log")
+    log.send(`${member} Keluar dari server ${member.guild.name} Owner server ${member.guild.owner.user.tag} !`)
+})
+  
+bot.on("channelCreate", channel => {
+  
+  if (channel.type == 'dm') return;
+  const log = bot.channels.find("name", "admin-log")
+  var embed = new Discord.RichEmbed()
+  .setTitle("Membuat Channel!")
   .setColor("RANDOM")
   .setTimestamp()
-  .setTimeout(process.exit, 1000 * 60 * 60 * 168)
-  .setThumbnail("http://freevector.co/wp-content/uploads/2009/03/40358-add-people-interface-symbol-of-black-person-close-up-with-plus-sign-in-small-circle.png")
-  .addField(`Saya telah memasuki server ${guild.name} Owned oleh ${guildOwner}`, `Sekarang jumlah server ${bot.guilds.size}, Dengan member ${bot.users.size} total!`)
-  log.send({ embed: embed2 })
+  .addField(`Nama channel : ${channel.name}`, `Channel dibuat diserver ${channel.guild.name}`)
+  log.send({ embed: embed })
 });
+  
+bot.on("channelDelete", channel => {
+  const log = bot.channels.find("name", "admin-log")
+  var embed = new Discord.RichEmbed()
+  .setTitle("Menghapus Channel!")
+  .setColor("RANDOM")
+  .setTimestamp()
+  .setThumbnail(`${channel.guild.iconURL}`)
+  .addField(`Nama channel : ${channel.name}`, `Channel dihapus diserver ${channel.guild.name}`)
+  log.send({ embed: embed })
+  });
+  
+bot.on("guildCreate", guild => {
+    const guildOwner = guild.owner.user.tag;
+    
+    const log = bot.channels.find("name", "server-log")
+    var embed2 = new Discord.RichEmbed()
+    .setTitle("Memasuki Server!")
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setTimeout(process.exit, 1000 * 60 * 60 * 168)
+    .setThumbnail("http://freevector.co/wp-content/uploads/2009/03/40358-add-people-interface-symbol-of-black-person-close-up-with-plus-sign-in-small-circle.png")
+    .addField(`Saya telah memasuki server ${guild.name} Owned oleh ${guildOwner}`, `Sekarang jumlah server ${bot.guilds.size}, Dengan member ${bot.users.size} total!`)
+    log.send({ embed: embed2 })
+});
+  
 
+bot.on("ready", async () => {
+    console.log(`${bot.user.tag} is ready!`);
 
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
+    function randomStatus() {
+        let status = [`Normal Mode!`, `i!invite`, `i!help`,`Remmove Music Commands!`, `${bot.guilds.size} Servers In The Your Party!`]
+        let rstatus = Math.floor(Math.random() * status.length);
+        bot.user.setActivity(status[rstatus], {type: 'Playing'});
 
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if (!prefixes[message.guild.id].prefix) prefixes[message.guild.id].prefix = "gb!"
-  if(commandfile) commandfile.run(bot,message,args);
+    }; setInterval(randomStatus, 10000)
 
+    bot.user.setStatus("dnd")
 });
 
 bot.login(process.env.BOT_TOKEN);
