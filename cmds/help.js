edcc2e0558d16e783b3456f12178dfc62853e0b7
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-21 20:44:49 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let helpembed = new Discord.RichEmbed()
   .setColor("#15f153")
-  .setDescription("**Prefix : `g!`**")
-  .addField(":lock: Moderators Command!", "| `g!ban [Player] [Reason]` | `g!kick [Player] [Reason]` | `g!tempmute [Player] [Time]` |\n| `g!say [say]` | `g!purge [Number]` | `g!news [news]` |\n| `g!warn [*Comming Soon*]` | `i!addrole [Player] [Role Name]` | `i!removerole help` | `i!createrole [Role Name]` |")
-  .addField(":earth_asia: General Command", "| `g!ping` |\n| `s!afk [Reason]` | `g!help` | `s!ask [question]` |\n| `g!userinfo [User]` | `g!stats` | `g!weather [Location]` |\n| `g!invite` | `g!ascii [text]`| `s!cat ` | `g!dog` ")
+  .setDescription("**Prefix : `cm!`**")
+  .addField(":lock: Moderators Command!", "| `cm!ban [Player] [Reason]` | `cm!kick [Player] [Reason]` | `cm!tempmute [Player] [Time]` |\n| `cm!say [say]` | `cm!purge [Number]` | `cm!warn [*Comming Soon*]` | `cm!addrole [Player] [Role Name]` | `cm!removerole help` | `cm!createrole [Role Name]` |")
+  .addField(":earth_asia: General Command", "| `cm!ping` |\n| `cm!afk [Reason]` | `cm!help` | `cm!ask [question]` |\n| `cm!userinfo [User]` | `cm!stats` | `cm!weather [Location]` |\n| `cm!invite` | `cm!ascii [text]`| ")
   .addField(":frame_photo: Photo Command", "| `g!dog` | `g!cat` |")
   .setFooter("Beta v0.2 | Discord.js");
   return message.channel.send(helpembed);
