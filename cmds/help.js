e42da51f3493e97454c84c757ea0278d94990a71
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-21 20:50:52 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   .setColor("#15f153")
   .setDescription("**Prefix : `cm!`**")
   .addField(":lock: Moderators Command!", "| `cm!ban [Player] [Reason]` | `cm!kick [Player] [Reason]` | `cm!tempmute [Player] [Time]` |\n| `cm!say [say]` | `cm!purge [Number]` | `cm!warn [*Comming Soon*]` | `cm!addrole [Player] [Role Name]` | `cm!removerole help` | `cm!createrole [Role Name]` |")
-  .addField(":earth_asia: General Command", "| `cm!ping` |\n| `cm!afk [Reason]` | `cm!help` | `cm!ask [question]` |\n| `cm!userinfo [User]` | `cm!stats` | `cm!weather [Location]` |\n| `cm!invite` | `cm!ascii [text]`| ")
+  .addField(":earth_asia: General Command", "| `cm!ping` |\n| `cm!afk [Reason]` | `cm!help` | `cm!ask [question]` |\n| `cm!userinfo [User]` | `cm!stats` | `cm!weather [Location]` |\n| `cm!invite` | `cm!ascii [text]` | `cm!mcstats [IP Server]` | `cm!mcuser [username]` ")
   .addField(":frame_photo: Photo Command", "| `g!dog` | `g!cat` |")
   .setFooter("Beta v0.2 | Discord.js");
   return message.channel.send(helpembed);
