eb472eb587e8f55a779b885868769d6f6867f23c
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-22 21:42:10 +0800

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,16 +1,18 @@
 const Discord = require("discord.js");
 
 exports.run = (bot, message, args) => {
+    message.channel.send("Pinging...").then(m => {
+        var lat_ms = (m.createdTimestamp - message.createdTimestamp);
+        var api_ms = (Math.round(bot.ping));
+        m.delete().then().catch(console.error);
     
-    var lat_ms = (m.createdTimestamp - message.createdTimestamp);
-	var api_ms = (Math.round(bot.ping));
-
-    let embed = new Discord.RichEmbed()
-    .addField("Latency", lat_ms + "ms", true)
-    .addField("API", api_ms + "ms", true)
-    .setTitle("Pong!")
-    .setColor(0x00AE86)
-    message.channel.send(embed);
+        let embed = new Discord.RichEmbed()
+        .addField("Latency", lat_ms + "ms", true)
+        .addField("API", api_ms + "ms", true)
+        .setTitle("Pong!")
+        .setColor(0x00AE86)
+        message.channel.send(embed);
+    });
 }
 
 exports.help = {
