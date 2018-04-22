97f8cdce0fdfbf0ef5e39b67aaca9b338f7d3e4f
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-22 21:31:12 +0800

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,25 +1,15 @@
 const Discord = require("discord.js");
 
 exports.run = (bot, message, args) => {
-		message.channel.send("Pinging...").then(m => {
-			var lat_ms = (m.createdTimestamp - message.createdTimestamp);
-			var api_ms = (Math.round(bot.ping));
-			m.delete().then().catch(console.error);
-			
-			const embed = new Discord.RichEmbed()
-			.setTitle("Pong!")
-			.setAuthor(config.handles.title)
-			.setColor(0x00AE86)
-			.addField("Latency", lat_ms + "ms", true)
-			.addField("API", api_ms + "ms", true)
-			.setFooter(`Developed by ${package.author} - Version ${package.version}`, config.handles.icon_url)
-			message.channel.send({embed});
-		}).catch(console.error);
+
+    let embed = new Discord.RichEmbed()
+    .addField("Latency", lat_ms + "ms", true)
+    .addField("API", api_ms + "ms", true)
+    .setTitle("Pong!")
+    .setColor(0x00AE86)
+    message.channel.send(embed);
 }
 
 exports.help = {
-    name: "ping",
-    description: "Shows the bots ping!",
-    usage: "pr!ping",
-    note: "Do I need to tell you what it does?"
+    name: "ping"
 }
