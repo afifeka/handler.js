ba32edd0c6796a2574d574025bcae7e57e4bdb5d
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-22 21:14:56 +0800

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -4,8 +4,9 @@ exports.run = (bot, message, args) => {
 
     let embed = new Discord.RichEmbed()
     .setTitle("PING")
-    .addField("Your Ping!", ` => **${message.client.ping}**`)
+    .addField(":signal_strength: | Your Ping!", ` => **${message.client.ping}**`)
     .setColor("#09fa0a")
+    .addField(":computer: | API Ping", `=> **${message.bot.ping}**`)
     message.channel.send(embed);
 }
 
