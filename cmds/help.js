62a1635b603e5e5ccc44cc7d95190f807b0fee38
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-23 21:37:08 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -8,8 +8,9 @@ module.exports.run = async (bot, message, args) => {
   .addField(":earth_asia: General Command", "| `cm!ping` |\n| `cm!afk [Reason]` | `cm!help` | `cm!ask [question]` |\n| `cm!userinfo [User]` | `cm!stats` | `cm!weather [Location]` |\n| `cm!invite` | `cm!ascii [text]` | `cm!mcstats [IP Server]` | `cm!mcuser [username]` ")
   .addField(":frame_photo: Photo Command", "| `g!dog` | `g!cat` |")
   .setFooter("Beta v0.2 | Discord.js");
-  return message.author.send(helpembed);
+  message.author.send(helpembed);
   message.react("☑");
+  return;
 
 }
 
