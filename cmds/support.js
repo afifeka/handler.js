0bb16d34ab2ed2fc4984b715dad5d2f10a2a5c89
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-24 10:17:11 +0800

    Update support.js

--- cmds/support.js
+++ cmds/support.js
@@ -3,14 +3,10 @@ const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
     let embed = new Discord.RichEmbed()
     .setTitle("SUPPORT!")
-    .addField("Discord Official", "[ https://discord.gg/aDuF567 ]")
-    .addField("Github", "[ Private ] ")
-    .addField("Owner", `@Afif_#9369 `)
-    .addField("Console Server", "[ Private ]")
-    .addField("Indonesian Developer Bot", "[ https://discord.gg/vgejeZB ]")
-    .addField("Vote And Invite!", "[ https://discordbots.org/bot/429589443486416906 ]")
-    .setColor("RANDOM")
-    .setFooter("Ada Command Yang Error? Segera Hubungi @Afif_9363")
+    .addField("Discord Official", "[**Click Here**](https://discord.gg/aDuF567)")
+    .addField("Indonesian Developer Bot", "[**Click Here**](https://discord.gg/vgejeZB)")
+    .addField("Vote And Invite!", `[**Click Here**](https://discordbots.org/bot/429589443486416906)`)
+    .setColor("#09fa2b")
     message.channel.send(embed)
 
 }
