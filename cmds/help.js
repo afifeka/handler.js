beb45c99536b120972d2799f3da893caca820eda
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-24 10:21:15 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -5,11 +5,12 @@ module.exports.run = async (bot, message, args) => {
   .setColor("#15f153")
   .setDescription("**Prefix : `cm!`**")
   .addField(":lock: Moderators Command!", "| `cm!ban [Player] [Reason]` | `cm!kick [Player] [Reason]` | `cm!tempmute [Player] [Time]` |\n| `cm!say [say]` | `cm!purge [Number]` | `cm!warn [*Comming Soon*]` | `cm!addrole [Player] [Role Name]` | `cm!removerole help` | `cm!createrole [Role Name]` |")
-  .addField(":earth_asia: General Command", "| `cm!ping` |\n| `cm!afk [Reason]` | `cm!help` | `cm!ask [question]` |\n| `cm!userinfo [User]` | `cm!stats` | `cm!weather [Location]` |\n| `cm!invite` | `cm!ascii [text]` | `cm!mcstats [IP Server]` | `cm!mcuser [username]` ")
-  .addField(":frame_photo: Photo Command", "| `g!dog` | `g!cat` |")
+  .addField(":earth_asia: General Command", "| `cm!ping` |\n| `cm!afk [Reason]` | `cm!help` | `cm!ask [question]` |\n| `cm!userinfo [User]` | `cm!stats` | `cm!weather [Location]` |\n| `cm!invite` | `cm!serverinfo` | `cm!ascii [text]` | `cm!mcstats [IP Server]` | `cm!mcuser [username]` ")
+  .addField(":frame_photo: Photo Command", "| `cm!dog` | `cm!cat` |")
+  .addField(":100: | New!", "| `cm!spotify [User]` |")
   .setFooter("Beta v0.2 | Discord.js");
   message.author.send(helpembed);
-  message.channel.send(`:mailbox_with_mail: | Plase Check Your DM's <@${message.author.id}>!`);
+  message.channel.send(`:mailbox_with_mail: Plase Check Your DM's <@${message.author.id}>!`);
   return;
 
 }
