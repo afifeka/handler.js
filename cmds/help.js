277827e96dc56bd89864837c3e0b9b3e0ccc8952
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-25 19:19:19 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,12 +3,12 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let helpembed = new Discord.RichEmbed()
   .setColor("#15f153")
-  .setDescription("**Prefix : `cm!`**")
-  .addField(":lock: Moderators Command!", "| `cm!ban [Player] [Reason]` | `cm!kick [Player] [Reason]` | `cm!tempmute [Player] [Time]` |\n| `cm!say [say]` | `cm!clear [Number]` | `cm!warn [user] [reason]` | `cm!addrole [Player] [Role Name]` | `cm!removerole help` | `cm!createrole [Role Name]` |")
-  .addField(":earth_asia: General Command", "| `cm!ping` |\n| `cm!afk [Reason]` | `cm!help` | `cm!ask [question]` |\n| `cm!userinfo [User]` | `cm!stats` | `cm!weather [Location]` |\n| `cm!invite` | `cm!serverinfo` | `cm!ascii [text]` | `cm!mcstats [IP Server]` | `cm!mcuser [username]` ")
-  .addField(":frame_photo: Photo Command", "| `cm!dog` | `cm!cat` |")
-  .addField(":100: | New!", "| `cm!spotify [User]` |")
-  .setFooter("Beta v0.2 | Discord.js");
+  .setDescription("**Prefix : `]`**")
+  .addField(":lock: Moderators Command!", "| `]ban [False]` | `]!kick [False]` | `]tempmute [Player] [Time]` |\n| `]say [say]` | `]clear [Number]` | `]warn [user] [reason]` | `]addrole [Player] [Role Name]` | `]removerole help` | `]createrole [Role Name]` |")
+  .addField(":earth_asia: General Command", "| `]ping` | `]afk [Reason]` | `]help` | `]ask [question]` |\n| `]userinfo [User]` | `]stats` | `]weather [Location]` |\n| `]invite` | `]serverinfo` | `]ascii [text]` | `]mcstats [IP Server]` | `]mcuser [username]` ")
+  .addField(":frame_photo: Photo Command", "| `]dog` | `]cat` |")
+  .addField(":100: | New!", "| `]spotify [User]` |")
+  .setFooter(" Tyrant | Beta v0.2 | Discord.js");
   message.author.send(helpembed);
   message.channel.send(`:mailbox_with_mail: Plase Check Your DM's <@${message.author.id}>!`);
   return;
