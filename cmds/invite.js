d7dd0a609cf34683fa6dd389b8a85d80e6ea84a7
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-27 21:17:50 +0700

    Update invite.js

--- cmds/invite.js
+++ cmds/invite.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
     .setDescription("***INVITE***")
-    .addField("**Invite To Yours Discord!**", "Link : https://discordapp.com/api/oauth2/authorize?client_id=429589443486416906&permissions=8&scope=bot")
+    .addField("**Invite To Yours Discord!**", "Link : Plase usage command ]support for invite and vote!")
     .setFooter("Cmd error? plase dm @Afif_#9369 now!")
     message.channel.send(embed)
 }
