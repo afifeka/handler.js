ee3593f0e187bc209312f7cd95d9e92ee6697c05
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-29 21:28:47 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,14 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let helpembed = new Discord.RichEmbed()
   .setColor("#15f153")
-  .setDescription("**Prefix : `]`**")
-  .addField(":lock: Moderators Command!", "| `]ban [False]` | `]!kick [False]` | `]tempmute [Player] [Time]` |\n| `]say [say]` | `]clear [Number]` | `]warn [user] [reason]` | `]addrole [Player] [Role Name]` | `]removerole help` | `]createrole [Role Name]` |")
-  .addField(":earth_asia: General Command", "| `]ping` | `]afk [Reason]` | `]help` | `]ask [question]` |\n| `]userinfo [User]` | `]stats` | `]weather [Location]` |\n| `]invite` | `]serverinfo` | `]ascii [text]` | `]mcstats [IP Server]` | `]mcuser [username]` ")
-  .addField(":frame_photo: Photo Command", "| `]dog` | `]cat` |")
-  .addField(":100: | New!", "| `]spotify [User]` | `]suggest [suggestion]` |")
-  .setFooter(" Tyrant | Beta v0.2 | Discord.js");
-  message.author.send(helpembed);
-  message.channel.send(`:mailbox_with_mail: Plase Check Your DM's <@${message.author.id}>!`);
+  .addField("**GENERAL**", "• `botinfo` to see bot information \n • `serverinfo` to see information server \n • `ping` see your ping")
+  message.channel.send(helpembed);
   return;
 
 }
