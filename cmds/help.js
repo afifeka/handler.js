e0683641b1c8f37b102d7e2fc826b4c9a30a1b70
afifeka <37196374+afifeka@users.noreply.github.com>
2018-05-02 21:11:21 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let helpembed = new Discord.RichEmbed()
   .setColor("#15f153")
-  .addField("**GENERAL**", "• `botinfo` to see bot information \n • `serverinfo` to see information server \n • `ping` see your ping")
+  .addDescreption("**GENERAL** \n • `botinfo` to see bot information \n • `serverinfo` to see information server \n • `ping` see your ping")
   message.channel.send(helpembed);
   return;
 
