642e349c63b19a90daf7821c21e2147bf06b47de
afifeka <37196374+afifeka@users.noreply.github.com>
2018-05-02 21:21:41 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,9 +3,17 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let helpembed = new Discord.RichEmbed()
   .setColor("#15f153")
-  .addDescreption("**GENERAL** \n • `botinfo` to see bot information \n • `serverinfo` to see information server \n • `ping` see your ping")
+  .setDescreption("**GENERAL** \n • `botinfo` to see bot information \n • `serverinfo` to see information server \n • `ping` see your ping")
   message.channel.send(helpembed);
+  if (args[0] == "2") {
+    return;
+  }
+  let helpembed2 = new Discord.RichEmbed()
+  .setColor("#15f153")
+  .setTitle("**MODERATION** \n • `ban` for banned player \n • `kick` for kick player \n • `warn` for warned the player")
+  message.channel.send(helpembed2);
   return;
+  
 
 }
 
