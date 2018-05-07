60c60297dbd99c078eee2f00ad40e0272dd77c88
afifeka <37196374+afifeka@users.noreply.github.com>
2018-05-07 19:56:39 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -2,10 +2,18 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Anda tidak ada permission untuk melakukan command ini.");
-  if(!args[0]) return message.channel.send("Silakan bagi saya number");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    let embed = new Discord.RichEmbed()
+    .setDescription("You dont have permission for run this command")
+    .setColor("#ce0e00")
+    return message.channel.send(embed);
+  if(!args[0]) {
+    let qembed = new Discord.RichEmbed()
+    .setDescription("Plase give me the numbers!")
+    .setColor("#ce0e00")
+    return message.channel.send(qembed);
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Sukses hapus ${args[0]} messages.`).then(msg => msg.delete(2000));
+  message.channel.send(`Cleared ${args[0]} messages!`).then(msg => msg.delete(2000));
 });
 
 }
