1ba252895c4edb9accfc8cf6965ca70ed80914eb
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-10 16:24:13 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -4,10 +4,10 @@ exports.run = (bot, message, args) => {
 const help = new Discord.RichEmbed()
 .setDescription("Prefix : =")
 .addField("**MODERATION**", " `ban`, `kick`, `warn`, `mute`, `addrole`, `removerole`, `clear`, `say`")
-.addField("**PUBLIC**", " `ping`, `ask`, `userinfo`, `stats`, `weather`, `support`, `suggest`, `ascii`, `mcstats`, `mcuser`")
+.addField("**PUBLIC**", " `ping`, `ask`, `userinfo`, `bigtext`, `stats`, `weather`, `support`, `suggest`, `ascii`, `mcstats`, `mcuser`")
 .addField("**IMAGE**", "`cat`, `dog`, `neko`")
 .addField("**MORE**", " `spotify`")
-.addField("**LINK**", "[Discord Server](https://discord.gg/xGp4ZAE), [Indonesian Official Development](https://discord.gg/XfNv2sZ)")
+.addField("**LINK**", "[Discord Server](https://discord.gg/xGp4ZAE), [Indonesian Official Development](https://discord.gg/XfNv2sZ), [Invite Me](https://discordapp.com/api/oauth2/authorize?client_id=454564831752618004&permissions=0&scope=bot)")
 .setColor("RANDOM")
 .setFooter(`${bot.user.username} | Discord.js`)
 
