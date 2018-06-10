5169f1c7d7727cc8a30f02b7cf2aca303be5c3d5
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-10 12:47:30 +0700

    Update ask.js

--- cmds/ask.js
+++ cmds/ask.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
 
 exports.run = (bot, message, args) => {
-    if(!args[2]) return message.reply("**Usage `]ask apakah <Question>`**");
-    let replies = ["Iya", "Tidak", "Saya Tidak Tahu", "Apa Yang Kamu Bilang?", "Sangat Benar", "Sangat Salah"];
+    if(!args[1]) return message.reply("**Usage `]ask <Question>`**");
+    let replies = ["Yes", "No", "I Don't Know", "What do you say?", "Very true", "You crazy?", "Whaaaat?"];
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.slice(1).join(" ");
 
     let ballembed = new Discord.RichEmbed()
     .setColor("#8d09f1")
-    .addField(":question: | Pertanyaan", question)
-    .addField(":envelope_with_arrow: | Jawaban", replies[result])
-    .setFooter(`Ask Request By ${message.author.tag} | Is Indonesian Langunge!`);
+    .addField("Question", question)
+    .addField("Answer", replies[result])
+    .setFooter(`For ${message.author.tag}`);
 
     message.channel.send(ballembed)
 }
