37879463dcbb3ce2ece73ba017be548d65ae1d5b
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-10 13:50:50 +0700

    Update createrole.js

--- cmds/createrole.js
+++ cmds/createrole.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(":warning: | Anda tidak ada permission untuk command ini!");
+    if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("You dont have permission for this command!");
     const name = message.content.split(' ').slice(1).join(' ');
-    if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.reply("Saya tidak ada permission untuk command ini!");
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.reply("I dont have permission for create role!");
     message.guild.createRole({
     name: `${name}`
     })
-    message.channel.send(`:white_check_mark: | Sukses membuat role ${name}!`)
+    message.channel.send(`Success Create Role ${name}!`)
 }
 
 module.exports.help = {
