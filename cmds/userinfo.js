7bea3cf5d872c98fa9c7331e22ec45ac64998594
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-11 15:08:59 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,16 +1,30 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+	
+	
+	    let game = user.presence.game && user.presence.game && user.presence.game.name
+ if (!game) {
+     game = "User is not playing a game!"
+ }
+ let status = {
+  "online": "<:online:449590947165110283> Online",
+  "idle": "<:away:449590947110584321> Idle",
+  "dnd": "<:dnd:449590946879766539> Do Not Disturb",
+  "invisible": "<:offline:449590947047669760> Offline"
+ }
     const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
     let embed = new Discord.RichEmbed()
     .setDescription("**USER INFO**")
     .setColor("#00a6ff")
     .setImage(member.user.displayAvatarURL)
-    .addField(":bust_in_silhouette: | Player", `${member.user.tag}`)
-    .addField(":shield: | ID", member.id)
-    .addField(":hammer: | Created", member.user.createdAt)
-    .addField(":inbox_tray: | Joined", member.joinedAt);
-
+    .addField("Player", `${member.user.tag}`)
+    .addField("ID", member.id)
+    .addField("Created", member.user.createdAt)
+    .addField("Joined Server", member.joinedAt)
+    .addField("Playing", game)
+    .addField("Status", `${status}`)
+    .addField("Nickname", message.member.displayName)
     message.channel.send(embed);
 
 }
