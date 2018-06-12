a514266ecb1e412ba43c9654ad543979baa32d9b
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-12 15:29:33 +0700

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 	    
-    let user = message.mentions.users.first() || message.author
+    let user = message.mentions.users.first() || message.author;
 	
 	
 	    let game = user.presence.game && user.presence.game && user.presence.game.name
@@ -16,6 +16,8 @@ module.exports.run = async (bot, message, args) => {
   "invisible": "<:offline:449590947047669760> Offline"
  }
     const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
+    let roles = member.roles.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(role => `<@&${role.id}>`);
+	
     let embed = new Discord.RichEmbed()
     .setDescription("**USER INFO**")
     .setColor("#00a6ff")
@@ -27,6 +29,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Playing", game)
     .addField("Status", status[user.presence.status])
     .addField("Nickname", message.member.displayName)
+    .addField("Roles", roles.join(', '))
     message.channel.send(embed);
 
 }
